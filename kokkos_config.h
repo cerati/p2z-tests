fefdd168ecb7110fd0a03cdba3133bbd5c64d7a5
Brian Gravelle <gravelle@cs.uoregon.edu>
2020-03-03 23:38:03 -0800

    improved GPU performance

--- kokkos_config.h
+++ kokkos_config.h
@@ -56,9 +56,9 @@
 typedef Kokkos::Cuda     ExecSpace;
 
 // typedef Kokkos::HostSpace     MemSpace;
-typedef Kokkos::OpenMP        MemSpace;
+// typedef Kokkos::OpenMP        MemSpace;
 // typedef Kokkos::CudaSpace     MemSpace;
-// typedef Kokkos::CudaUVMSpace  MemSpace;
+typedef Kokkos::CudaUVMSpace  MemSpace;
 
 typedef Kokkos::LayoutLeft   Layout;
 // typedef Kokkos::LayoutRight  Layout; // faster for CPUs
